9b9beaeae0f76f8aad031d19207694b400198542
guilhermejacob <guilhermejacob91@gmail.com>
2020-01-18 18:22:32 -0300

    atualiza versão

--- R/svyflow.R
+++ R/svyflow.R
@@ -52,7 +52,7 @@
 #' @export
 #' @rdname svyflow
 #' @method svyflow survey.design2
-svyflow.survey.design2 <- function( x , design , flow.type = c( "gross" , "net" ) , rounds = c(0,1) , max.iter = 10 , ... ){
+svyflow.survey.design2 <- function( x , design , flow.type , rounds , max.iter , ... ){
 
   # collect data
   xx <- lapply( design$variables[ rounds + 1 ] , function( z ) stats::model.frame( x , data = z , na.action = na.pass ) )
@@ -110,22 +110,20 @@ svyflow.survey.design2 <- function( x , design , flow.type = c( "gross" , "net" 
   } )
 
   # create matrix of contrasts and z variables
-  xxf <- model.matrix( ~-1+. , data = xx , contrasts.arg = lapply( xx , contrasts, contrasts=FALSE ) , na.action = na.pass )
-  oo <- matrix( 0 , nrow = nrow(xx) , ncol = ncol(xxf) , dimnames = list( seq_len( nrow(xx) ) , NULL ) )
-  oo[ match( rownames( xxf ) , rownames( oo ) ) , ] <- xxf
-  xxf <- oo ; rm( oo )
   zz <- apply( xx , 2 , function(z) as.numeric( !is.na(z) ) )
 
   ### special variables
   vv_k <- rowSums( yy[[1]] ) * rowSums( yy[[2]] ) + rowSums( yy[[2]] * (1 - zz[,1]) ) + rowSums( yy[[1]] * (1 - zz[,2]) )
-  nnij_k <- lapply( seq_len( ncol( yy[[1]] ) ) , function(z) yy[[1]][ , z ] * yy[[2]] )
+  nnij_k <- list(NULL)
+  for ( i in seq_len( nrow(NN) ) ) nnij_k[[i]] <- list(NULL)
+  for ( i in seq_len( nrow(NN) ) ) for ( j in seq_len( ncol( NN ) ) ) nnij_k[[i]][[j]] <- yy[[1]][,i] * yy[[2]][,j]
 
   # u variables
   ueta_ik <- list(NULL)
   for ( i in seq_len( nrow(NN) ) ) {
     ueta_ik[[i]] <-
-      ( rowSums( nnij_k[[i]] ) + yy[[1]][,i, drop = FALSE ] * ( 1 - zz[,2 , drop = FALSE ] ) ) / eta_iv [ i ] +
-      rowSums( sweep( yy[[2]] * ( 1 - zz[,1] ) , 2 , p_ijv[i,] / colSums( nipij ) , "*" ) ) +
+      ( rowSums( do.call( cbind , nnij_k[[i]] ) ) + yy[[1]][,i, drop = FALSE ] * ( 1 - zz[,2 , drop = FALSE ] ) ) / eta_iv [i] +
+      rowSums( sweep( sweep( yy[[2]] , 1 , ( 1 - zz[,1] ) , "*" ) , 2 , p_ijv[i,] / colSums( nipij ) , "*" ) ) +
       (1 - zz[,1]) * (1 - zz[,2])
   }
   ueta_ik <- do.call( cbind , ueta_ik)
@@ -133,8 +131,8 @@ svyflow.survey.design2 <- function( x , design , flow.type = c( "gross" , "net" 
   up_ijk <- list(NULL)
   for ( i in seq_len( nrow(NN) ) ) up_ijk[[i]] <- list(NULL)
   for ( i in seq_len( nrow(NN) ) ) for ( j in seq_len( ncol( NN ) ) ) {
-    up_ijk[[i]][[j]] <- nnij_k[[i]][,j] / p_ijv[i,j] + yy[[1]][,i] * (1 - zz[,2]) +
-      yy[[2]][,j] * (1 - zz[,1]) * ( eta_iv[i] * colSums( nipij )[j] ) + (1 - zz[,1]) * (1 - zz[,2]) * eta_iv[i]
+    up_ijk[[i]][[j]] <- nnij_k[[i]][[j]] / p_ijv[i,j] + yy[[1]][,i] * (1 - zz[,2]) +
+      yy[[2]][,j] * (1 - zz[,1]) * ( eta_iv[i] / colSums( nipij )[j] ) + (1 - zz[,1]) * (1 - zz[,2]) * eta_iv[i]
   }
 
   # j variables
@@ -148,41 +146,46 @@ svyflow.survey.design2 <- function( x , design , flow.type = c( "gross" , "net" 
   jp_ij <- list(NULL)
   for ( i in seq_len( nrow(NN) ) ) jp_ij[[i]] <- list(NULL)
   for ( i in seq_len( nrow(NN) ) ) for ( j in seq_len( ncol( NN ) ) ) {
-    jp_ij[[i]][[j]] <- - (1/p_ijv[i,j])^2 * sum( ww * yy[[1]][ , i ] * yy[[2]][ , j ] ) - ( eta_iv[i] / rowSums( nipij^2 )[j] ) * sum( ww * yy[[2]][,j] * (1 - zz[,1]) )
-  }
-
-  # corrects u variables; i.e., u / j
-  ueta_ik <- sweep( ueta_ik , 2 , jeta_i ,  "/" )
-  for ( i in seq_len( nrow(NN) ) ) for ( j in seq_len( ncol( NN ) ) ) {
-    up_ijk[[i]][[j]] <- up_ijk[[i]][[j]] / jp_ij[[i]][[j]]
+    jp_ij[[i]][[j]] <- - 1/(p_ijv[i,j]^2) * sum( ww * nnij_k[[i]][[j]] ) - ( ( eta_iv[i] / colSums( nipij )[j] )^2 ) * sum( ww * yy[[2]][,j] * (1 - zz[,1] ) )
   }
 
   # variance of NN
   NNvar <- matrix( NA, nrow = nrow(NN) , ncol = ncol(NN) )
   for ( i in seq_len( nrow(NN) ) ) for ( j in seq_len( ncol( NN ) ) ) {
-    NNvar[i,j] <- survey::svyrecvar( nnij_k[[i]][,j] * ww , clusters = design$cluster , stratas = design$strata , fpcs = design$fpc , postStrata = design$postStrata )
+    NNvar[i,j] <- survey::svyrecvar( ww * nnij_k[[i]][[j]] , clusters = design$cluster , stratas = design$strata , fpcs = design$fpc , postStrata = design$postStrata )
   }
 
   # variance of eta and p-matrix
-  eta_var <- survey::svyrecvar( ueta_ik * ww , clusters = design$cluster , stratas = design$strata , fpcs = design$fpc , postStrata = design$postStrata )
+  eta_var <- survey::svyrecvar( ww * ueta_ik , clusters = design$cluster , stratas = design$strata , fpcs = design$fpc , postStrata = design$postStrata )
+  eta_var <- diag(eta_var) / jeta_i^2
   p_var <- matrix( NA, nrow = nrow(NN) , ncol = ncol(NN) )
   for ( i in seq_len( nrow(NN) ) ) for ( j in seq_len( ncol( NN ) ) ) {
-    p_var[i,j] <- survey::svyrecvar( up_ijk[[i]][[j]] * ww , clusters = design$cluster , stratas = design$strata , fpcs = design$fpc , postStrata = design$postStrata )
+    p_var[i,j] <- survey::svyrecvar( ww * up_ijk[[i]][[j]] , clusters = design$cluster , stratas = design$strata , fpcs = design$fpc , postStrata = design$postStrata )
+    p_var[i,j] <- p_var[i,j] / jp_ij[[i]][[j]]^2
   }
 
   # a variables
   mu_var <- matrix( NA, nrow = nrow(NN) , ncol = ncol(NN) )
   for ( i in seq_len( nrow(NN) ) ) for ( j in seq_len( ncol( NN ) ) ) {
     a7 <- nipij[i,j]
-    a8 <- nipij[i,j]
-    a9 <- nipij[i,j]
-    mu_var[i,j] <- a7^2 * NNvar[i,j] + a8^2 * eta_var[i,i] + a9^2 * p_var[i,j]
+    a8 <- NN[i,j] * p_ijv[i,j]
+    a9 <- NN[i,j] * eta_iv[i]
+    mu_var[i,j] <- (a7^2) * NNvar[i,j] + (a8^2) * eta_var[i] + (a9^2) * p_var[i,j]
   }
+
+  # #alternative strategy
+  # mu_var <- matrix( NA, nrow = nrow(NN) , ncol = ncol(NN) )
+  # for ( i in seq_len( nrow(NN) ) ) for ( j in seq_len( ncol( NN ) ) ) {
+  #   nipij_k <- p_ijv[i,j] * ( ueta_ik[,i] / jeta_i[[i]] ) + eta_iv[i] * ( up_ijk[[i]][[j]] / jp_ij[[i]][[j]] )
+  #   muij_k <- nipij[i,j] * nnij_k[[i]][[j]] + NN[i,j] * nipij_k
+  #   # mu_var[i,j] <- survey::svyrecvar( ww * nipij_k , clusters = design$cluster , stratas = design$strata , fpcs = design$fpc , postStrata = design$postStrata )
+  #   mu_var[i,j] <- survey::svyrecvar( ww * muij_k , clusters = design$cluster , stratas = design$strata , fpcs = design$fpc , postStrata = design$postStrata )
+  # }
 
   # format results
   rval <- if ( flow.type == "gross" ) mu_ij else nipij
   class(rval) <- "flowstat"
-  attr( rval , "var" )       <- if ( flow.type == "gross" ) mu_var else mu_var / N^2
+  attr( rval , "var" )       <- if ( flow.type == "gross" ) mu_var else mu_var / NN^2
   dimnames( attr( rval , "var" ) ) <- dimnames( mu_ij )
   attr( rval , "statistic" ) <- flow.type
   attr( rval , "rounds" )    <- rounds
@@ -194,7 +197,7 @@ svyflow.survey.design2 <- function( x , design , flow.type = c( "gross" , "net" 
 #' @export
 #' @rdname svyflow
 #' @method svyflow svyrep.design
-svyflow.svyrep.design <- function( x , design , flow.type = c( "gross" , "net" ) , rounds = c(0,1) , max.iter = 10 , ... ){
+svyflow.svyrep.design <- function( x , design , flow.type , rounds , max.iter , ... ){
 
   # collect data
   xx <- lapply( design$variables[ rounds + 1 ] , function( z ) stats::model.frame( x , data = z , na.action = na.pass ) )
@@ -292,8 +295,8 @@ svyflow.svyrep.design <- function( x , design , flow.type = c( "gross" , "net" )
   p_var <- rval
   p_var[,] <- NA
   for ( i in seq_len( nrow(NN) ) ) for ( j in seq_len( ncol( NN ) ) ) {
-    qq <- sapply( reps , function(zz) zz[ i , j] )
-    p_var[i,j] <- survey::svrVar( qq , design$scale , design$rscales[ !null_reps ] , mse = design$mse , coef = rval )
+    qq <- sapply( reps , function(zz) zz[ i , j ] )
+    p_var[i,j] <- survey::svrVar( qq , design$scale , design$rscales[ !null_reps ] , mse = design$mse , coef = rval[i,j] )
   }
 
   # format results
@@ -310,15 +313,16 @@ svyflow.svyrep.design <- function( x , design , flow.type = c( "gross" , "net" )
 #' @export
 #' @rdname svyflow
 #' @method svyflow survflow.design
-svyflow.survflow.design <- function( x , design , ... ) NextMethod( "svyflow" , design )
+svyflow.survflow.design <- function( x , design , flow.type = c( "gross" , "net" ) , rounds = c(0,1) , max.iter = 10 , ... ) {
+  flow.type <- match.arg( flow.type )
+  if ( !all( rounds %in% c(0, seq_along( design$variables ) ) ) ) stop( "rounds not in range." )
+  NextMethod( "svyflow" , design = design , flowtype , rounds , max.iter , ... )
+}
 
 #' @export
 svyflow <- function( x , design , ... ) {
   # test valid arguments
-  flow.type <- match.arg( flow.type )
   if ( class(x) != "formula" ) stop( "x must be a formula." )
   if ( length(x) != 2 ) stop( "x must be a single variable." )
-  if ( length(rounds) != 2 ) stop( "rounds must have length = 2." )
-  if ( !all( rounds %in% c(0, seq_along( design$variables ) ) ) ) stop( "rounds not in range." )
   UseMethod( "svyflow" , design )
 }
